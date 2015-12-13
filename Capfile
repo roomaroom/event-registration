40884f578d1d1bad108fd23f904d72b9d3380207
roomaroom <olegk@hnd.sm.ua>
2015-12-13 15:58:33 +0200

    add capfile

--- Capfile
+++ Capfile
@@ -3,7 +3,12 @@ require 'capistrano/setup'
 
 # Includes default deployment tasks
 require 'capistrano/deploy'
-
+require 'capistrano/rvm'
+require 'capistrano/bundler'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
+require 'capistrano3/unicorn'
+require 'capistrano/linked_files'
 # Includes tasks from other gems included in your Gemfile
 #
 # For documentation on these, see for example:
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # set :deploy_to, '/var/www/my_app'
@@ -16,25 +13,52 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
+# config valid only for Capistrano 3.1
+#lock '3.4.0'
+set :application, 'Scholl'
+set :repo_url, 'https://github.com/roomaroom/event-registration.git'
+set :shared_path, "/home/school/production/school/shared"
+set :branch, 'master'
+set :scm, :git
+set :deploy_to, '/home/school/production/school'
+# set :format, :pretty
+# set :pty, true
+set :preload_app, true
+set :default_stage, 'production'
+set :rails_env, "production"
+#set :use_sudo, false
+#set :deploy_via, :copy
+set :deploy_via, :remote_cache
+#set :log_level, :info
+set :linked_files, %w(config/database.yml)
+# Default value for :scm is :git
+# set :scm, :git
+set :unicorn_config_path, "/home/school/production/kyc.uno/school/config/unicorn.rb"
+set :unicorn_pid, "/home/school/production/school/shared/pids/unicorn.pid"
+set :linked_dirs, %w{ bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads }
+# Default value for :format is :pretty
+# set :format, :pretty
+#set :port, 2225
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
-  desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+    invoke 'unicorn:restart'
   end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
 end
