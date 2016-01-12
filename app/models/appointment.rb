6aba41df0d2e0b0500188a7cfc57e672e0a3c549
administrator <oleg.kozlovskyi@perfectial.com>
2016-01-12 14:10:03 +0200

    remove elasticsearch

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,32 +1,32 @@
 class Appointment < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  #include Elasticsearch::Model
+  #include Elasticsearch::Model::Callbacks
 
   belongs_to :user
   belongs_to :event
-  
+
   default_scope { order('created_at DESC') }
 
   enum payment: { 'Повна вартість 100грн'=> 100, 'Для студентів 70грн'=> 70, 'Без Харчування 40грн'=> 40, 'Не маю можливості заплатити'=> 0 }
 
-  def as_indexed_json(options={})
-    as_json(
-      only: [:id, :notice],
-      include: [ :user ]
-    )
-  end
+#   def as_indexed_json(options={})
+#     as_json(
+#       only: [:id, :notice],
+#       include: [ :user ]
+#     )
+#   end
 
-  def self.search(query)
-  __elasticsearch__.search(
-    {
-      query: {
-        query_string: {
-          query: "*#{query}"
-        }
-      }
-    }
-  )
-end
+#   def self.search(query)
+#   __elasticsearch__.search(
+#     {
+#       query: {
+#         query_string: {
+#           query: "*#{query}"
+#         }
+#       }
+#     }
+#   )
+# end
 
   # settings index: { number_of_shards: 1 } do
   #   mappings dynamic: 'false' do
@@ -34,7 +34,7 @@ end
   #   end
   # end
 
-  def self.search_with_elasticsearch(*args)
-    __elasticsearch__.search(*args)
-  end
+  # def self.search_with_elasticsearch(*args)
+  #   __elasticsearch__.search(*args)
+  # end
 end
